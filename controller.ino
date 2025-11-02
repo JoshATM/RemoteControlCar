c15ecd010293e77e28a7e61f79200d1aea35b408
JoshATM <114391172+JoshATM@users.noreply.github.com>
2025-11-02 03:52:27 +0000

    Updated controller.ino
    Refactor motor control functions and update WiFi credentials.

--- controller.ino
+++ controller.ino
@@ -1,14 +1,14 @@
 #include <WiFi.h>
 
-const char* ssid = "WIFI NAME";
-const char* password = "WIFI PASSWORD";
+const char* ssid = "wifi name";
+const char* password = "wifi pass";
+
+const int motor1_forward = 2;
+const int motor1_backward = 3;
+const int motor2_forward = 4;
+const int motor2_backward = 5;
 
 WiFiServer server(80);
-
-const int motor1_forward = 3;
-const int motor1_backward = 2;
-const int motor2_forward = 5;
-const int motor2_backward = 4;
 
 void setup() {
   Serial.begin(115200);
@@ -18,64 +18,92 @@ void setup() {
   pinMode(motor2_forward, OUTPUT);
   pinMode(motor2_backward, OUTPUT);
   
+  stopMotors();
+  
+  Serial.println("Connecting to WiFi...");
   WiFi.begin(ssid, password);
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   
-  Serial.println("\nConnected");
-  Serial.print("IP: ");
+  Serial.println();
+  Serial.println("WiFi connected!");
+  Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
   
   server.begin();
+  Serial.println("Server started");
 }
 
 void loop() {
   WiFiClient client = server.available();
   
   if (client) {
-    String command = "";
+    Serial.println("Client connected");
     
     while (client.connected()) {
       if (client.available()) {
-        char c = client.read();
-        if (c == '\n') {
-          command.trim();
-          executeCommand(command);
-          client.println("OK");
-          command = "";
-        } else {
-          command += c;
+        String command = client.readStringUntil('\n');
+        command.trim();
+        
+        Serial.print("Command: ");
+        Serial.println(command);
+        
+        if (command == "FORWARD") {
+          moveForward();
+        } else if (command == "BACKWARD") {
+          moveBackward();
+        } else if (command == "LEFT") {
+          turnLeft();
+        } else if (command == "RIGHT") {
+          turnRight();
+        } else if (command == "STOP") {
+          stopMotors();
         }
+        
+        client.println("OK");
       }
     }
+    
     client.stop();
+    Serial.println("Client disconnected");
+    stopMotors();
   }
 }
 
-void executeCommand(String cmd) {
+void MoveForward() {
+  digitalWrite(motor1_forward, LOW);
+  digitalWrite(motor1_backward, HIGH);
+  digitalWrite(motor2_forward, LOW);
+  digitalWrite(motor2_backward, HIGH);
+}
+
+void MoveBackward() {
+  digitalWrite(motor1_forward, HIGH);
+  digitalWrite(motor1_backward, LOW);
+  digitalWrite(motor2_forward, HIGH);
+  digitalWrite(motor2_backward, LOW);
+}
+
+void TurnLeft() {
+  digitalWrite(motor1_forward, HIGH);
+  digitalWrite(motor1_backward, LOW);
+  digitalWrite(motor2_forward, LOW);
+  digitalWrite(motor2_backward, HIGH);
+}
+
+void TurnRight() {
+  digitalWrite(motor1_forward, LOW);
+  digitalWrite(motor1_backward, HIGH);
+  digitalWrite(motor2_forward, HIGH);
+  digitalWrite(motor2_backward, LOW);
+}
+
+void StopMotors() {
   digitalWrite(motor1_forward, LOW);
   digitalWrite(motor1_backward, LOW);
   digitalWrite(motor2_forward, LOW);
   digitalWrite(motor2_backward, LOW);
-  
-  if (cmd == "FORWARD") {
-    digitalWrite(motor1_forward, HIGH);
-    digitalWrite(motor2_forward, HIGH);
-  } 
-  else if (cmd == "BACKWARD") {
-    digitalWrite(motor1_backward, HIGH);
-    digitalWrite(motor2_backward, HIGH);
-  } 
-  else if (cmd == "LEFT") {
-    digitalWrite(motor1_backward, HIGH);
-    digitalWrite(motor2_forward, HIGH);
-  } 
-  else if (cmd == "RIGHT") {
-    digitalWrite(motor1_forward, HIGH);
-    digitalWrite(motor2_backward, HIGH);
-  }
-  
-  Serial.println(cmd);
 }
